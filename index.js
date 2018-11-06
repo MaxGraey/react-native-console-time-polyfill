f9bffc36dc2a13fae6b2e65e1923902240d1a002
MaxGraey <maxgraey@gmail.com>
2018-11-06 12:33:43 +0200

    improve fallbacks for native timers

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
 'use strict'
 
-import fbPerformanceNow from 'fbjs/lib/performanceNow';
-const PerformanceNow = global.nativePerformanceNow || global.performanceNow || fbPerformanceNow;
+const PerformanceNow =
+  global.nativeQPLTimestamp   ||
+  global.nativePerformanceNow ||
+  require('fbjs/lib/performanceNow');
 
 let counts     = {};
 let startTimes = {};
 
 console.time = console.time || (label => {
-    startTimes[label] = PerformanceNow();
+  startTimes[label] = PerformanceNow();
 });
 
 console.timeEnd = console.timeEnd || (label => {
-    let endTime = PerformanceNow();
-    if (startTimes[label]) {
-        let delta = endTime - startTimes[label];
-        console.log(`${label}: ${delta.toFixed(3)}ms`);
-        delete startTimes[label];
-    } else {
-        console.warn(`Warning: No such label '${label}' for console.timeEnd()`);
-    }
+  let endTime = PerformanceNow();
+  if (startTimes[label]) {
+    let delta = endTime - startTimes[label];
+    console.log(`${label}: ${delta.toFixed(3)}ms`);
+    delete startTimes[label];
+  } else {
+    console.warn(`Warning: No such label '${label}' for console.timeEnd()`);
+  }
 });
 
 console.count = console.count || ((label = '<no label>') => {
-    if (!counts[label])
-        counts[label] = 0;
-	
-    counts[label]++;
-    console.log(`${label}: ${counts[label]}`);
+  if (!counts[label])
+    counts[label] = 0;
+
+  counts[label]++;
+  console.log(`${label}: ${counts[label]}`);
 });
