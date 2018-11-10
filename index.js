a6817fc46cff138db383a629868777d379aa51c5
MaxGraey <maxgraey@gmail.com>
2018-11-11 00:06:55 +0200

    add console.resetCount. change default label and messeges

--- index.js
+++ index.js
@@ -5,28 +5,39 @@ const PerformanceNow =
   global.nativePerformanceNow ||
   require('fbjs/lib/performanceNow');
 
+const DEFAULT_LABEL = 'default';
+const DEFAULT_PREC  = 3;
+
 let counts     = {};
 let startTimes = {};
 
-console.time = console.time || (label => {
+console.time = console.time || ((label = DEFAULT_LABEL) => {
   startTimes[label] = PerformanceNow();
 });
 
-console.timeEnd = console.timeEnd || (label => {
+console.timeEnd = console.timeEnd || ((label = DEFAULT_LABEL) => {
   let endTime = PerformanceNow();
   if (startTimes[label]) {
     let delta = endTime - startTimes[label];
-    console.log(`${label}: ${delta.toFixed(3)}ms`);
+    console.log(`${label}: ${delta.toFixed(DEFAULT_PREC)}ms`);
     delete startTimes[label];
   } else {
-    console.warn(`Warning: No such label '${label}' for console.timeEnd()`);
+    console.warn(`Timer '${label}' does not exist`);
   }
 });
 
-console.count = console.count || ((label = '<no label>') => {
+console.count = console.count || ((label = DEFAULT_LABEL) => {
   if (!counts[label])
     counts[label] = 0;
 
   counts[label]++;
   console.log(`${label}: ${counts[label]}`);
+});
+
+console.countReset = console.countReset || ((label = DEFAULT_LABEL) => {
+  if (counts[label]) {
+    counts[label] = 0;
+  } else {
+    console.warn(`Count for '${label}' does not exist`);
+  }
 });
